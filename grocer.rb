3f286be040508c557adcfbeb3c30932663a1cabf
Angel Alanis <a4alaniz@gmail.com>
2019-09-15 02:08:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,10 +36,9 @@ end
 
 def apply_clearance(cart)
   cart.each do |product_name, stats|
-   stats[:price] -= stats[:price] * 0.2 if stats[:clearance]
+   stats[:price] -= stats[:price] * 0.2 if stats[:clearance] binding.pry
  end
  cart
- binding.pry
 end
 
 def checkout(cart, coupons)
